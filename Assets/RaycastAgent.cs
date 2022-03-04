993ed93d4cf2702e371a43a76ca3877b472dc9df
nicknick08 <derekchiu12@gmail.com>
2022-03-04 15:54:28 +0900

    LSTM

--- Assets/RaycastAgent.cs
+++ Assets/RaycastAgent.cs
@@ -13,6 +13,10 @@ public class RaycastAgent : Agent
     public Transform[] targets;
     public GameObject[] boards;
     int boardId;
+    
+
+
+    
 
     // 初期化時に呼ばれる
     public override void Initialize()
@@ -43,34 +47,9 @@ public class RaycastAgent : Agent
 
         }
 
-
-
-
-        // // curiosity
-        // this.lastCheckPoint = 0;
-        // this.checkPointCount = 0;
-
-
-
-
-        // // RaycastAgentが床から落下している時
-        // if (this.transform.localPosition.y < 0)
-        // {
-        //     // RaycastAgentの位置と速度をリセット
-        //     this.rBody.angularVelocity = Vector3.zero;
-        //     this.rBody.velocity = Vector3.zero;
-        //     this.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
-        // }
-
-        // // Targetの位置のリセット
-        // target.localPosition = new Vector3(
-        //     Random.value*8-4, 0.5f, Random.value*8-4);
     }
 
-    // public override void CollectObservations(VectorSensor sensor){
-    //     sensor.AddObservation(rBody.velocity.x); // RollerAgentのX速度
-    //     sensor.AddObservation(rBody.velocity.z); // RollerAgentのZ速度
-    // }
+   
 
     // 行動実行時に呼ばれる
     public override void OnActionReceived(float[] vectorAction)
@@ -99,53 +78,11 @@ public class RaycastAgent : Agent
             
         }
         AddReward(-0.0005f);
+        
 
-
-    //     // RaycastAgentに力を加える
-    //     Vector3 dirToGo = Vector3.zero;
-    //     Vector3 rotateDir = Vector3.zero;
-    //     int action = (int)vectorAction[0];
-    
-    //     if(action == 1) dirToGo = transform.forward;
-    //     if(action == 2) dirToGo = transform.forward *-1.0f;
-    //     if(action == 3) rotateDir = transform.up *-1.0f;
-    //     if(action == 4) rotateDir = transform.up;
-    //     this.transform.Rotate(rotateDir, Time.deltaTime*200f);
-    //     // this.transform.position+=dirToGo*Time.deltaTime*2.0f;
-    //     this.rBody.AddForce(dirToGo * 0.4f, ForceMode.VelocityChange);
-
-
-    //     // // RaycastAgentがTargetの位置に到着した時
-    //     // float distanceToTarget = Vector3.Distance(
-    //     //     this.transform.localPosition, target.localPosition);
-
-    //   AddReward(-0.001f);
-
-        // if (distanceToTarget < 1.42f){
-        //     AddReward(1.0f);
-        //     EndEpisode();
-        // }
     }
 
 
-                //  curiosity
-
-    // public void EnterCheckPoint(int checkPoint){
-    //     if(checkPoint == (this.lastCheckPoint+1)%4){
-    //         this.checkPointCount++;
-
-    //         if(this.checkPointCount >= 4){
-    //             AddReward(2.0f);
-    //             EndEpisode();
-    //         }
-    //     }else if(checkPoint == (this.lastCheckPoint - 1 +4 )%4){
-    //         this.checkPointCount--;
-    //     }
-
-
-    //     this.lastCheckPoint = checkPoint;
-    // }
-    // ヒューリスティックモードの行動決定時に呼ばれる
     public override void Heuristic(float[] actionsOut)
     {
         actionsOut[0]=0;
